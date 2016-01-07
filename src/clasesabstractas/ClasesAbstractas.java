7acbcd6be03d306a1497b430171698157961bd49
jbarrosgarcia <jbarrosgarcia@atanasoff07.danielcastelao.org>
2016-01-07 12:28:27 +0100

    Renombre las variables y añadi los metodos que me faltaban y corregi un par de fallos que tenia el programa

---
+++ src/clasesabstractas/ClasesAbstractas.java
@@ -0,0 +1,15 @@
+
+
+package clasesabstractas;
+
+
+public class ClasesAbstractas {
+
+    
+    public static void main(String[] args) {
+        Moto moto1=new Moto();
+        System.out.println(moto1.arrancar());
+    
+    }
+    
+}
